7cfb8e6097f25c87328e011db49ed7c36e305541
Marc Canaleta <marc@cloudnumbers.net>
2024-03-19 19:50:49 +0100

    publish script

--- scripts/newversion.sh
+++ scripts/newversion.sh
@@ -6,26 +6,27 @@ set -x
 
 pnpm run build
 # Array of package names
+base_path="packages"
+
 packages=("common" "server" "client" "utils")
 
 # Function to check changes, update version, and publish
 update_and_publish() {
     local package_name=$1
-    cd "packages/${package_name}"
+    cd "${base_path}/${package_name}"
 
-    # Check if there are changes in the package
-    if [[ -n $(git status -s) ]]; then
-        # If changes exist, patch version, commit, and publish
+    # Check if there are changes in the package directory since the last commit
+    git status --porcelain .    
+    if git status --porcelain . | grep -q .; then
+        echo "CHANGES in ${package_name}"
         npm version patch
         git add .
         git commit -m "new version of ${package_name}"
-        pnpm publish
-    else
-        echo "No changes in ${package_name}, skipping."
+        pnpm publish --access public
     fi
 
     # Return to the root directory
-    cd ../..
+    cd ../../
 }
 
 # Iterate over each package and apply updates if necessary
